cf06a11a41f1524f5b345a471ebf9d3164468f09
Stephen <anil.stephen66@gmail.com>
2019-03-28 22:40:47 +0530

    updated the font-family

--- functions.php
+++ functions.php
@@ -1,4 +1,4 @@
 <?php
 add_action('wp_enqueue_scripts',function(){
-  wp_enqueue_style('oxfam-style',get_stylesheet_directory_uri().'/style.css',array('sp-core-style'),time());
+  wp_enqueue_style('oxfam-style', get_stylesheet_directory_uri().'/style.css', array('sp-core-style'), '1.0.0' );
 });

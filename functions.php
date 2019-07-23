5d896c88e46da2920a74694c1e119b2b2f253865
Steve <anil.stephen66@gmail.com>
2019-07-23 15:30:51 +0530

    updated style

--- functions.php
+++ functions.php
@@ -1,4 +1,5 @@
 <?php
 add_action('wp_enqueue_scripts',function(){
-  wp_enqueue_style('oxfam-style', get_stylesheet_directory_uri().'/style.css', array('sp-core-style'), '1.0.0' );
+
+  wp_enqueue_style('oxfam-style', get_stylesheet_directory_uri().'/style.css', array(), '1.0.0' );
 });
